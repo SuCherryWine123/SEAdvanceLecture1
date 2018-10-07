038fb5033e13a55f64b5709936a84aa7d6d4b53c
Acer\Aspire <cherryy552@gmail.com>
2018-10-07 15:38:58 +0630

    Lecture1 added.

---
+++ Lecture1/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {1,2,3,4,5};
+		int max = 0;
+		for(int i= 0; i < 6; i++) {
+				if(i > max) {
+					max = i;
+					
+				}
+			}
+		System.out.println(max);
+		}
+}
